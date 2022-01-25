b3284fbcbae16bde43eb5087f75fef7a36170f7e
Jamaril24 <jamarlopez24@g.crookcountyschools.org>
2022-01-25 09:57:51 -0800

    added

---
+++ script.js
@@ -0,0 +1,15 @@
+'use script'
+
+console.log('hello');
+
+document.write(<h1>Arrays Demo</h1>)
+
+const drinks = ("Coffee", "Water", "Tea")
+
+console.log(drinks);
+
+document.write( drinks[1]);
+
+drinks.push("Juice");
+
+console.log(drinks);
